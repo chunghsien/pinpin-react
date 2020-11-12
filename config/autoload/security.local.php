65bf24dc5cba7d3dce192116ae2d9495aa282fff
chunghsien <wowowo.jacky@gmail.com>
2020-11-12 21:55:20 +0800

    加入一些金鑰的設定檔(主要是mysql aes的key)

---
+++ config/autoload/security.local.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'encryption' => [
+        'jwt_key' => 'dZBQejLMnE',
+        'jwt_alg' => 'HS256',
+        'aes_key' => '{v^c2iz.',
+        'charlist' => '`1234567890-=~!@#$%^&*()_+qwertyuiop[]\QWERTYUIOP{}|asdfghjkl;\'ASDFGHJKL:"zxcvbnm,./ZXCVBNM<>?',
+    ]
+];
